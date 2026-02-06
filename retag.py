9241874a20e7f9deea1d866cc998f8549ac7822f
Klaus Zerwes <zerwes@users.noreply.github.com>
2026-02-06 12:38:14 +0100

    retag

--- retag.py
+++ retag.py
@@ -22,4 +22,54 @@ parser = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
 
+parser.add_argument('-t', '--tags',
+    help='tag to remove',
+    nargs='+',
+    action="append",
+    required=True
+    )
+parser.add_argument('-a', '--addtags',
+    help='tag to add to all tickets that have the old tag',
+    nargs='+',
+    action="append",
+    )
+
 zammadl = zammadls.zammadls.Zammadl(parser)
+
+# flatten tag list
+removetags = [item for sublist in zammadl.args.tags for item in sublist]
+logger.info('will remove tags %s', ', '.join(removetags))
+
+addtags = []
+if zammadl.args.addtags:
+    # flatten tag list
+    addtags = [item for sublist in zammadl.args.addtags for item in sublist]
+if addtags:
+    logger.info('will add tags %s', ', '.join(addtags))
+    for ntag in addtags:
+        zammadl.zammad.taglist.create({'name': ntag})
+
+# check if tags exists
+logger.info('fetching all tags from instance ...')
+alltags = zammadl.zammad.taglist.all()
+alltagnames = set([t['name'] for t in alltags])
+rtags = list(set(removetags) - alltagnames)
+for t in rtags:
+    logger.warn('tag %s not found, ignoring', t)
+    removetags.remove(t)
+if rtags:
+    logger.info('remaining tags to remove: %s', ', '.join(removetags))
+
+for rtag in removetags:
+    # fetch all tickets
+    tickets = zammadl.zammad.ticket.search(f'tags:{rtag}')
+    logger.info('tag %s : found %s tickets', rtag, len(tickets))
+    for ticket in tickets:
+        logger.debug('ticket id:%s ...', ticket['id'])
+        if not zammadl.args.dryrun:
+            zammadl.zammad.ticket_tag.remove(ticket['id'], rtag)
+        logger.debug(' ... removed tag %s', rtag)
+        for ntag in addtags:
+            if not zammadl.args.dryrun:
+                zammadl.zammad.ticket_tag.add(ticket['id'], ntag)
+            logger.debug(' ... added tag %s', ntag)
